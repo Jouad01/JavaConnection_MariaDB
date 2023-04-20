f842216f1afb5cf60dae21a45f098e5ff346566e
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-04-20 16:53:19 +0200

    feat: menu connection with jdbc&jdbc pool

--- src/main/java/edu/daw/JDBC/JDBC_Example.java
+++ src/main/java/edu/daw/JDBC/JDBC_Example.java
@@ -8,7 +8,6 @@ public class JDBC {
         String user = "root";
         String password = "1234";
 
-//   statement
         Connection conn = null;
         Statement stmt = null;
 
@@ -16,17 +15,16 @@ public class JDBC {
             // conexion con la base de datos
             conn = DriverManager.getConnection(url, user, password);
 
-            // Creación del statement
             stmt = conn.createStatement();
 
             // consulta SQL
-            String sql ="INSERT INTO user (first_name, last_name, email, birthday) values ('jouad', 'el ouardi', 'ola@gmial.com', '1984-07-10') "; // Consulta para crear una tabla
-            stmt.executeUpdate(sql); // Ejecución de la consulta
+            String sql ="INSERT INTO user (first_name, last_name, email, birthday) values ('jouad', 'el ouardi', 'ola@gmial.com', '1984-07-10') ";
+            stmt.executeUpdate(sql);
 
             stmt.close();
             conn.close();
 
-            System.out.println("Insert realizado con éxito");
+            System.out.println("Insert with success");
         } catch (SQLException e) {
             System.err.println("Error: " + e.getMessage());
         }
--- src/main/java/edu/daw/JDBC/JDBC_POOL.java
+++ src/main/java/edu/daw/JDBC/JDBC_POOL.java
@@ -1,43 +1,147 @@
 package edu.daw.JDBC;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class JDBC_POOL {
+    private static HikariDataSource dataSource;
+
     public static void main(String[] args) throws SQLException {
-        // Configuración del pool de conexiones
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/delete_me");
-        dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+        initDatabaseConnectionPool();
+        try {
+            boolean exit = false;
+            Scanner sc = new Scanner(System.in);
 
-        // Obtener conexión del pool
-        Connection conn = dataSource.getConnection();
+            while (!exit) {
+                System.out.println("\n=== Menu ===");
+                System.out.println("1. Add user");
+                System.out.println("2. Update email");
+                System.out.println("3. Delete user");
+                System.out.println("4. Show users");
+                System.out.println("0. Exit");
+                System.out.println("Select an option");
+                int option = sc.nextInt();
 
-        // Ejecutar una consulta
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM example");
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter first name: ");
+                        String first_name = sc.next();
+                        System.out.println("Enter last name: ");
+                        String last_name = sc.next();
+                        System.out.println("Enter email: ");
+                        String email = sc.next();
+                        System.out.println("Enter birthday (yyyy-MM-dd: )");
+                        String birthdayStr = sc.next();
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                        LocalDate birthday = LocalDate.parse(birthdayStr, formatter);
+                        createUser(first_name,last_name,email, birthday);
+                        break;
+                    case 2:
+                        System.out.println("Enter the first name of the user to update: ");
+                        String nameToUpdate = sc.next();
+                        System.out.println("Enter the new email: ");
+                        String newEmail = sc.next();
+                        updateData(nameToUpdate,newEmail);
+                        break;
+                    case 3:
+                        System.out.println("Enter the first name of the user to delete: ");
+                        String nameToDelete = sc.next();
+                        deleteData(nameToDelete);
+                        break;
+                    case 4:
+                        readUser();
+                        break;
+                    case 0:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid option, please try again.");
+                }
+            }
+            sc.close();
+        } finally {
+            closeDatabaseConnectionPool();
+        }
+    }
+    private static void createUser(String first_name, String last_name, String email, LocalDate birthday) throws SQLException {
+        System.out.println("Creating user...");
+        int rowsInserted;
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO user (first_name, last_name, email, birthday) VALUES (?, ?, ?, ?)"))
+            {
+                statement.setString(1, first_name);
+                statement.setString(2, last_name);
+                statement.setString(3, email);
+                statement.setDate(4, Date.valueOf(birthday));
+                rowsInserted = statement.executeUpdate();
+            }
+        }
+        System.out.println("Files inserted: " + rowsInserted);
+    }
 
-        // Recorrer los resultados
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            System.out.println(id + ", " + name);
+    private static void readUser() throws SQLException {
+        System.out.println("Reading all users...");
+        try  (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM user")) {
+                ResultSet resultSet = statement.executeQuery();
+                boolean empty = true;
+                while (resultSet.next()) {
+                    int user_id = resultSet.getInt(1);
+                    String first_name = resultSet.getString(2);
+                    String last_name = resultSet.getString(3);
+                    String email = resultSet.getString(4);
+                    LocalDate birthday = resultSet.getObject(5, LocalDate.class);
+                    System.out.println("\t> User id:" + user_id + ", fist name: " + first_name + ", last name: " + last_name + ", email: " + email + ", birthday: " + birthday);
+                    empty = false;
+                }
+                if (empty) {
+                    System.out.println("\t (no data)");
+                }
+            }
+        }
+    }
+
+    private static void updateData(String first_name, String newEmail) throws SQLException {
+        System.out.println("Data update...");
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(" UPDATE user SET email = ? WHERE first_name = ?"))
+            {
+                statement.setString(1, newEmail);
+                statement.setString(2, first_name);;
+                int rowsUpdated = statement.executeUpdate();
+                System.out.println("Rows updated: " + rowsUpdated);
+
+            }
+        }
+    }
+
+    private static void deleteData(String nameExpression) throws SQLException {
+        System.out.println("Deleting data...");
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("DELETE FROM user WHERE first_name LIKE ?"))
+            {
+                statement.setString(1, nameExpression);
+                int rowsDeleted = statement.executeUpdate();
+                System.out.println("Deleted rows: " + rowsDeleted);
+            }
         }
 
-        // Insertar un nuevo registro
-        String insertQuery = "INSERT INTO example (name) VALUES ('John')";
-        int rowsAffected = stmt.executeUpdate(insertQuery);
-        System.out.println("Filas afectadas: " + rowsAffected);
-
-        // Cerrar recursos y conexión
-        rs.close();
-        stmt.close();
-        conn.close();
     }
+
+    public static void initDatabaseConnectionPool()  {
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mariadb://localhost:3306/library");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+    }
+
+    public static void closeDatabaseConnectionPool() {
+        dataSource.close();
+
+    }
+
 }
